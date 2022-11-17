ef95331d00d09c3282da476eb1d2027b54bbf9a9
chaitu364590 <84063946+chaitu364590@users.noreply.github.com>
2022-11-17 11:46:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,12 +4,14 @@ import tensorflow as tf
 import os
 import numpy as np
 import PIL
+import pandas as pd
+from PIL import Image,ImageEnhance
 ## Page Title
 #st.set_page_config(page_title = "Cats vs Dogs Image Classification")
 st.title("CONTACTLESS VITAL SIGN DETECTION")
 st.markdown("------")
 
-model_path='vitals.tflite'
+model_path='C:/Users/lenovo/POTHOLE/vitals.tflite'
 
 
 
@@ -18,7 +20,48 @@ classes = ['watch glass', 'conical flask','cork','bunsen burners','corks','testt
 #label_map = model.model_spec.config.label_map
 #for label_id, label_name in label_map.as_dict().items():
  # classes[label_id-1] = label_name
+uploaded_video = st.file_uploader("Choose video", type=["mp4", "mov"])
+frame_skip = 300 # display every 300 frames
 
+if uploaded_video is not None: # run only when user uploads video
+    vid = uploaded_video.name
+    with open(vid, mode='wb') as f:
+        f.write(uploaded_video.read()) # save video to disk
+
+    st.markdown(f"""
+    ### Files
+    - {vid}
+    """,
+    unsafe_allow_html=True) # display file name
+
+    vidcap = cv2.VideoCapture(vid) # load video from disk
+    cur_frame = 0
+    success = True
+    
+
+    while success:
+        success, frame = vidcap.read() # get next frame from video
+        if cur_frame % frame_skip == 0: # only analyze every n=300 frames
+            print('frame: {}'.format(cur_frame)) 
+            pil_img = Image.fromarray(frame) # convert opencv frame (with type()==numpy) i
+            st.image(pil_img)
+            cur_frame += 1
+            our_image_A = pil_img##tview
+            st.image(our_image_A)
+            our_image_A=np.array(our_image_A.convert('RGB'))
+            gray = cv2.cvtColor(our_image_A, cv2.COLOR_BGR2GRAY)
+            st.image(gray)
+
+            gray8_image=np.zeros((120, 160), dtype=np.uint8)
+            gray8_image=cv2.normalize(gray, gray8_image,0,255,cv2.NORM_MINMAX)
+            gray8_image=np.uint8(gray8_image)
+            inferno_palette=cv2.applyColorMap(gray8_image, cv2.COLORMAP_INFERNO)
+            jet_palette=cv2.applyColorMap(gray8_image, cv2.COLORMAP_JET)
+            viridis_palette=cv2.applyColorMap(gray8_image, cv2.COLORMAP_VIRIDIS)
+            st.image(gray8_image)
+            st.image(inferno_palette)
+            st.image(jet_palette)
+            st.image(viridis_palette)
 # Define a list of colors for visualization
 COLORS = np.random.randint(0, 255, size=(len(classes), 3), dtype=np.uint8)
 
@@ -118,9 +161,9 @@ def run_odt_and_draw_results(image_path, interpreter, threshold=0.3):
 ## Input Fields
 uploaded_file = st.file_uploader("Upload a Image", type=["jpg","png", 'jpeg'])
 if uploaded_file is not None:
-    with open(os.path.join("/tmp",uploaded_file.name),"wb") as f:
+    with open(os.path.join("C:/Users/lenovo/POTHOLE",uploaded_file.name),"wb") as f:
         f.write(uploaded_file.getbuffer())
-    path = os.path.join("/tmp",uploaded_file.name)
+    path = os.path.join("C:/Users/lenovo/POTHOLE",uploaded_file.name)
     URL =path
     DETECTION_THRESHOLD = 0.3
 
@@ -139,3 +182,16 @@ if uploaded_file is not None:
     st.image(detection_result_image)
 
  
+
+
+
+
+
+
+
+
+
+
+
+
+
